ad00004c25fba2d675dbd3d942bb2786727b8283
Michał Nowacki <michal.nowacki@reef.pl>
2025-02-17 16:10:38 +0100

    hotfix

--- app/src/project/settings.py
+++ app/src/project/settings.py
@@ -421,9 +421,6 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.TokenAuthentication",
         "rest_framework.authentication.SessionAuthentication",
     ],
-    "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.IsAuthenticated",
-    ],
     "DEFAULT_RENDERER_CLASSES": [
         "rest_framework.renderers.JSONRenderer",
         "rest_framework.renderers.BrowsableAPIRenderer",
